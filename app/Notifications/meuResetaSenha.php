4e4e258e5e85bf1016c33bf60227f6222f3b70b1
paulosantama <paulovitor_pe@hotmail.com>
2019-02-15 10:22:54 -0200

    reinicio de senha

---
+++ app/Notifications/meuResetaSenha.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class meuResetaSenha extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Notificação de Redefinição de Senha.')
+                    ->greeting('Olá!')
+                    ->line('Você está recebendo este e-mail porquê nós recebemos uma solicitação de redefinição de senha para sua conta.')
+                    ->action('Redefinir Senha', route('password.reset', $this->token))
+                    ->line('Este link de redefinição de senha expirará em 60 minutos.')
+                    ->line('Se você não solicitou uma redefinição de senha, nenhuma ação é necessária.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
